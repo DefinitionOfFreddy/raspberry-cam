2145b2afa4b2d32e8195100809fe69b033f3fcf8
Penel Frédéric <penel.frederic@gmail.com>
2014-07-17 22:27:42 +0200

    compar_cercle

--- code/cercle.h
+++ code/cercle.h
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include "type.h"
 
-int coord_centre(img I, pixel* p, uint8 c)
+int coord_centre(img I, pixel* p, uint8 c);
+
+int compar_cercle(img I, img J, uint8 c);
 
 #endif
