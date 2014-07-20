6ce1eb90f6366ba0ad629d180f7d0b2acf5b7a7c
Antoine Honore <antoinehonor@gmail.com>
2014-07-20 10:20:54 +0200

    Ajout de commentaire et d'une fonction (desallocation_1) pour libérer la mémoire occupé par une image noir et blanc ou niveau de gris

--- code/imp_exp.h
+++ code/imp_exp.h
@@ -1,5 +1,5 @@
-#ifndef IMPORT_
-#define IMPORT_
+#ifndef IMPORT_EXPORT
+#define IMPORT_EXPORT
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -7,6 +7,7 @@
 #include "type.h"
 
 img import_1(char* f, int*, int*);     // .pbm (noir & blanc) ou .pgm (niveau gris)-> matrice
+int desallocation_1(img I, int lig);
 int export_1(img , char* f, int ext, int lig , int col); //Matrice -> .pbm ou .pbm
 imgRGB import_RGB(char* f, int*, int* );         //.ppm (couleur) -> matrice RGB
 int export_RGB(imgRGB Irgb, char* f, int lig , int col); //Matrice -> .ppm
--- code/testcercle.c
+++ code/testcercle.c
@@ -11,10 +11,12 @@ void main (int argc, char* argv[])
 	char* f;
 	f="centre_noir_50x50.pbm";
 
-	I = import_1(f, &nlig, &ncol);
+	I = import_1(f, &nlig, &ncol); //Allocation de I et lecture du fichier nommé f
 	if (I) printf("Importation ok\n");
 	affiche_img1(I,nlig,ncol);
 
 	export = export_1(I,"essai.pbm",0,nlig,ncol);
 	if (export) printf("Exportation ok\n");
+	desallocation_1(I,nlig);
+
 }
