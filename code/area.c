c31d156a34d4f05d986245a7cbdbb94f4d447f27
Antoine Honore <antoinehonor@gmail.com>
2014-07-25 09:20:57 +0200

    Commentaires et debut_area_1 non testée

--- code/area.c
+++ code/area.c
@@ -2,7 +2,17 @@
 
 int debut_area_1(img I, int nlig, int ncol, int c, double seuil)
 {
-
+	int i,j;
+	int compt=0;
+	for (i=0;i<ncol;i++)
+	{
+		for (j=0;j<nlig;j++)
+		{
+			if (I[i][j].couleur == c) compt++;
+		}
+		if (compt/nlig>=seuil) return i;
+	}
+	return -1;
 }
 
 int fin_area_1(img I, int nlig, int ncol, int c, double seuil, int debut)
--- code/area.h
+++ code/area.h
@@ -5,6 +5,8 @@
 #include <stdio.h>
 
 int debut_area_1(img I, int nlig, int ncol, int c, double seuil);
+//Balaie l'image de gauche à droite et renvoie le numéro de la première colonne où l'on a plus de seuil% de couleur c
 int fin_area_1(img I, int nlig, int ncol, int c, double seuil, int debut);
+//Balaie l'image à partir de debut_area_1 et renvoie le numéro de la première colonne où l'on a moins de seuil% de couleur c
 
 #endif
