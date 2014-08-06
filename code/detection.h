4e527ffcfaf04bce86db1ca21b8a3a51d11f959e
Antoine Honore <antoinehonor@gmail.com>
2014-08-06 16:14:48 +0200

    detection récursive d'une zone detection.c

---
+++ code/detection.h
@@ -0,0 +1,15 @@
+#ifndef DETECTION_
+#define DETECTION_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#include "type.h"
+#include "imp_exp.h"
+
+
+int recherche(img_RGB Irgb, int i, int j, int ncol, int nlig, pixel_RGB* pref, double eps);
+
+
+#endif
